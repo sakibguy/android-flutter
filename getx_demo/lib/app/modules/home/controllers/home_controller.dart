e9ec432ac442f22a17c1cffd7e5b08ddca0e83ce
sakibguy <sakib.rahman.0000@gmail.com>
2022-09-18 13:25:20 +0600

    Add GetConnect and StateMixin

--- getx_demo/lib/app/modules/home/controllers/home_controller.dart
+++ getx_demo/lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,21 @@
 import 'package:get/get.dart';
+import 'package:getx_demo/app/modules/home/providers/UserProvider.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with StateMixin<List<dynamic>> {
   final RxInt count = 0.obs; // RxInt(0);
   void increment() => count.value++;
 
+  final UserProvider _userProvider = UserProvider();
   @override
   void onInit() {
     print('[---ok---] HomeController has been created!');
+
+    _userProvider.getUsers().then((response) {
+      print(response);
+      change(response, status: RxStatus.success());
+    }, onError: (error) {
+      change(null, status: RxStatus.error(error.toString()));
+    });
 
     count.value = 10;
     super.onInit();
@@ -14,7 +23,7 @@ class HomeController extends GetxController {
 
   @override
   void onClose() {
-    // print('[---ok---] HomeController has been deleted!');
+    print('[---ok---] HomeController has been closed!');
 
     count.value = 0;
     super.onClose();
