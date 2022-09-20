f2dc49c8c6f1b8dccea7209ad0d8ab51763f8921
sakibguy <sakib.rahman.0000@gmail.com>
2022-09-20 21:26:54 +0600

    Add udacity-course/03_category_route

---
+++ udacity-course/03_category_route/task_03_category_route/android/app/src/main/java/com/example/task_03_category_route/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.task_03_category_route;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
