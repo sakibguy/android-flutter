baa183d00d9d15b1417e475b3f572460debf76c7
sakibguy <sakib.rahman.0000@gmail.com>
2022-10-19 16:57:33 +0600

    Practice StatefulWidget

---
+++ flutter_usecases/1-manage-states/practice_syntax_from_0/lib/main.dart
@@ -0,0 +1,148 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'BP: State Management',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: "SMDemo HomePage"),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   final String title;
+//
+//   const MyHomePage({super.key, required this.title});
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Text("You have pushed the button this many times."),
+//             Text(widget.title),
+//             Text(widget.title),
+//             Text(widget.title),
+//             Text(widget.title),
+//             Text('$_counter'),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MA Practice',
+      home: const MyStateApp(title: "SW Demo Showing"),
+    );
+  }
+}
+
+class MyStateApp extends StatefulWidget {
+  final String title;
+  const MyStateApp({super.key, required this.title});
+
+  @override
+  State<MyStateApp> createState() => _MyStateAppState();
+}
+
+class _MyStateAppState extends State<MyStateApp> {
+  int _counter = 0;
+
+  // Define method: way 1
+  void _increaseCounter() => {
+    setState(() => {
+      _counter++
+    })
+  };
+
+  // Define method: way 2
+  // void _increaseCounter() => {
+  //   setState(() {
+  //     _counter++;
+  //   })
+  // };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')))
+            },
+            icon: Icon(Icons.arrow_forward_ios_rounded),
+            tooltip: 'Arrow',
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Text 1"),
+            Text("Text 2"),
+            Text('$_counter'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          _increaseCounter()
+        },
+        tooltip: 'FA tooltip',
+        child: const Icon(Icons.add)
+      ),
+    );
+  }
+}
