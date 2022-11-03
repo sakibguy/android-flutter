45324d483ec0947d0db16be57edc058ff1c46a6b
sakibguy <sakib.rahman.0000@gmail.com>
2022-11-03 16:41:40 +0600

    Add Controller provider getx view litst

--- flutter_usecases/1-manage-states/practice_syntax_from_0/lib/main.dart
+++ flutter_usecases/1-manage-states/practice_syntax_from_0/lib/main.dart
@@ -1,113 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'controller/news_controller.dart';
-import 'model/news_response.dart';
+import 'package:get/get.dart';
+import 'package:practice_syntax_from_0/routes/routes.dart';
+import 'package:practice_syntax_from_0/views/home_view.dart';
 
-void main() => runApp(MyApp());
+import 'binding/home_binding.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "News App",
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Discover",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Find intresting article and news",
-                style: TextStyle(color: Colors.grey),
-              ),
-              SizedBox(height: 20),
-              Expanded(
-                child: FutureBuilder<List<Article>>(
-                  future: NewsController().fetchNewsArticle(),
-                  builder: (context, snapshot) {
-                    var data = snapshot.data;
-
-                    if (!snapshot.hasData) {
-                      print("[---ok---] ifdata: $data");
-                      return Center(child: CircularProgressIndicator());
-                    } else {
-                      print("[---ok---] elsedata: $data");
-
-                      List<Article>newsArticle = snapshot.data!;
-                      return ListView.builder(
-                        itemCount: newsArticle.length,
-                        itemBuilder: (context, index) {
-                          return NewsTile(article: newsArticle[index]);
-                        },
-                      );
-                    }
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
+void main() => runApp(
+  GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Get MA",
+    getPages: [
+      GetPage(
+          name: Routes.HOME,
+          page: () => HomeView(),
+          binding: HomeBinding(),
       ),
-    );
-  }
-}
-
-class NewsTile extends StatelessWidget {
-  final Article article;
-
-  const NewsTile({Key? key, required this.article}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(7),
-      child: ListTile(
-        onTap: () async {
-          await canLaunch(article.url)
-              ? await launch(article.url)
-              : throw 'Could not launch ${article.url}';
-        },
-        title: Text(
-          article.title,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-        ),
-        subtitle: Text(
-          article.description,
-          maxLines: 4,
-          overflow: TextOverflow.ellipsis,
-        ),
-        leading: article.urlToImage != null
-            ? Container(
-          width: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: NetworkImage(article.urlToImage),
-            ),
-          ),
-        )
-            : null,
-      ),
-    );
-  }
-}
+      GetPage(name: Routes.LOGIN, page: () => Container()),
+      GetPage(name: Routes.SIGNUP, page: () => Container())
+    ],
+    initialRoute: Routes.INITIAL,
+  )
+);
---
+++ flutter_usecases/1-manage-states/practice_syntax_from_0/lib/providers/UserProvider.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class UserProvider extends GetConnect {
+  Future<List<dynamic>> getUsers() async {
+    final response = await get('https://randomuser.me/api/?results=20');
+
+    if (response.status.hasError) {
+      return Future.error(response.statusText!);
+    } else {
+      return response.body['results'];
+    }
+  }
+}
